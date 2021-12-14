83d403270e8b4c8cb8ae962947cc4440cb9ffb96
takingupspace <belllabenthusiast@gmail.com>
2021-12-14 11:02:29 -0600

    need-to-add-google-api-key

--- new-web-app/public/javascripts/delicious-app.js
+++ new-web-app/public/javascripts/delicious-app.js
@@ -1,3 +1,7 @@
 import '../sass/style.scss';
-
+import autocomplete from './modules/autocomplete';
+import autocompelte from './modules/autocomplete'
 import { $, $$ } from './modules/bling';
+
+autocomplete($('#address'), $('#lat'), $('#lng'));
+
